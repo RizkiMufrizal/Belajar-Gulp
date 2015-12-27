92840e812fdefccedc5f1d4b48bdc7761a4b843a
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-27 14:02:47 +0700

    tambah konfigurasi minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var gulpMinifyCss = require('gulp-minify-css');
+var gulpConcat = require('gulp-concat');
+var gulpUglify = require('gulp-uglify');
 
 gulp.task('minify-css', function() {
   gulp.src('./src/index.css')
@@ -7,4 +9,15 @@ gulp.task('minify-css', function() {
       compatibility: 'ie8'
     }))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify-js', function() {
+  gulp
+    .src([
+      './src/index1.js',
+      './src/index2.js'
+    ])
+    .pipe(gulpConcat('bundle.js'))
+    .pipe(gulpUglify())
+    .pipe(gulp.dest('dist'));
 });
