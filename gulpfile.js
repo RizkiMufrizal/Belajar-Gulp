f4618fdfae219458d63843f437cf8b1f2ee9c30d
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-27 14:03:35 +0700

    tambah konfigurasi minify html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gulpMinifyCss = require('gulp-minify-css');
 var gulpConcat = require('gulp-concat');
 var gulpUglify = require('gulp-uglify');
+var gulpHtmlmin = require('gulp-htmlmin');
 
 gulp.task('minify-css', function() {
   gulp.src('./src/index.css')
@@ -20,4 +21,12 @@ gulp.task('minify-js', function() {
     .pipe(gulpConcat('bundle.js'))
     .pipe(gulpUglify())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-html', function() {
+  gulp.src('src/*.html')
+    .pipe(gulpHtmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'))
 });
